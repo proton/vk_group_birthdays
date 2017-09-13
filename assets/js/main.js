6d5d955f434d4421316db9867da0a0c9b13f00c3
Peter Savichev (proton) <psavichev@gmail.com>
2017-09-14 00:49:13 +0300

    on model change event

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var VK_API_VERSION = '5.61';
+
 function auth() {
   VK.Auth.getLoginStatus(function(response) {
     if (!response.session) {
@@ -14,6 +16,7 @@ function auth() {
     }
   });
 }
+
 VK.init({ apiId: 6183713 });
 auth();
 
@@ -22,12 +25,48 @@ var app = new Vue({
   data: {
     url: 'https://vk.com/s1ovesnik',
     users: [],
+    group_info: null
+  },
+  watch: {
+    url: function() { this.updateData(); }
+  },
+  created: function() { this.updateData(); },
+  methods: {
+    resetData: function() {
+      this.users = [];
+      this.group_info = null;
+    },
+    updateData: function() {
+      if (this.urlInvalid()) this.resetData();
+      else {
+        this.loadGroupInfo();
+      }
+    },
     urlInvalid: function() {
       var domain = 'https://vk.com/';
       if(_.startsWith(this.url, domain)) {
         return false;
       }
       return true;
+    },
+    loadGroupInfo: function() {
+      var self = this;
+      var domain = 'https://vk.com/';
+      var group_id = this.url.slice(domain.length);
+      console.log(group_id);
+      VK.Api.call('groups.getById', { group_id: group_id, fields: 'members_count, photo_50', v: VK_API_VERSION }, function(r) {
+        if(r.response) {
+          self.group_info = r.response[0];
+          //
+
+          // $('.group_info')
+          // .html('<img src="' + r.response[0].photo_50 + '"/><br/>' 
+          //   + r.response[0].name
+          //   + '<br/>Участников: ' + r.response[0].members_count);
+          // getMembers20k(group_id, r.response[0].members_count); // получаем участников группы и пишем в массив membersGroups
+        }
+        else self.resetData();
+      });
     }
   }
 })
