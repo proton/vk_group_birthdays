5de77877d2a5085983c9b89f851c1ec9444a7f66
Peter Savichev (proton) <psavichev@gmail.com>
2017-09-14 10:07:41 +0300

    first release!

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,11 @@ auth();
 var app = new Vue({
   el: '#app',
   data: {
+    domain: 'https://vk.com/',
     url: 'https://vk.com/s1ovesnik',
     users: [],
-    group_info: null
+    group_info: null,
+    progress: 100
   },
   watch: {
     url: function() { this.updateData(); }
@@ -35,6 +37,7 @@ var app = new Vue({
     resetData: function() {
       this.users = [];
       this.group_info = null;
+      this.progress = 100;
     },
     updateData: function() {
       if (this.urlInvalid()) this.resetData();
@@ -43,29 +46,53 @@ var app = new Vue({
       }
     },
     urlInvalid: function() {
-      var domain = 'https://vk.com/';
-      if(_.startsWith(this.url, domain)) {
+      if(_.startsWith(this.url, this.domain)) {
         return false;
       }
       return true;
     },
     loadGroupInfo: function() {
       var self = this;
-      var domain = 'https://vk.com/';
-      var group_id = this.url.slice(domain.length);
-      console.log(group_id);
-      VK.Api.call('groups.getById', { group_id: group_id, fields: 'members_count, photo_50', v: VK_API_VERSION }, function(r) {
+      var group_id = this.url.slice(self.domain.length);
+      VK.Api.call('groups.getById', { group_id: group_id, fields: 'members_count, photo_100', v: VK_API_VERSION }, function(r) {
         if(r.response) {
           self.group_info = r.response[0];
-          //
-
-          // $('.group_info')
-          // .html('<img src="' + r.response[0].photo_50 + '"/><br/>' 
-          //   + r.response[0].name
-          //   + '<br/>Участников: ' + r.response[0].members_count);
-          // getMembers20k(group_id, r.response[0].members_count); // получаем участников группы и пишем в массив membersGroups
+          if (self.group_info.members_count) self.loadGroupMembers();
         }
         else self.resetData();
+      });
+    },
+    loadGroupMembersApiRequest: function(group_id, count, offset) {
+      return 'API.groups.getMembers({"group_id": ' + group_id + ', "v": '+VK_API_VERSION+', "sort": "id_asc", "fields": "photo_50, photo_400_orig, education, universities, schools, bdate", "count": "' + count + '", "offset": ' + offset + '}).items';
+    },
+    loadGroupMembers: function() {
+      var self = this;
+      var group_id = self.group_info.id;
+      var members_count = self.group_info.members_count;
+      self.progress = self.users.length*100/members_count;
+      var offset = 1000;
+      var code =  'var members = ' + self.loadGroupMembersApiRequest(group_id, offset, self.users.length) + ';' // делаем первый запрос и создаем массив
+      + 'var offset = '+offset+';' // это сдвиг по участникам группы
+      + 'while (offset < '+ 25*offset + ' && (offset + ' + self.users.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
+      + '{'
+        + 'members = members + ' + self.loadGroupMembersApiRequest(group_id, offset, self.users.length + ' + offset') + ';' // сдвиг участников на offset + мощность массива
+        + 'offset = offset + '+offset+';' // увеличиваем сдвиг на 1000
+      + '};'
+      + 'return members;'; // вернуть массив members
+  
+      VK.Api.call("execute", {code: code}, function(r) {
+        if (r.response) {
+          console.log(r.response[0]);
+          self.users = self.users.concat(r.response); // запишем это в массив
+          self.progress = self.users.length*100/members_count;
+          if (members_count >  self.users.length) // если еще не всех участников получили
+            setTimeout(function() { self.loadGroupMembers(); }, 333); // задержка 0.333 с. после чего запустим еще раз
+          else // если конец то
+            self.progress = 100;
+        } else {
+          console.log(data.error.error_msg);
+          self.resetData();
+        }
       });
     }
   }
